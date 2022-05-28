f513ad89db0059d262f9dc9a28a213831d614736
unknown <anasyunusind@gmail.com>
2022-05-28 20:39:53 +0530

    reload bug, clr()

--- script.js
+++ script.js
@@ -9,14 +9,30 @@ let dayNumber = 0
 let fappedNumber = 0
 let savedNumber = 0
 let percent = 0
+let dateNumberJS = new Date().getDate()
+let monthNumberJS = new Date().getMonth()
 let arr = []
 let savedData = []
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"]
+
+
 document.getElementById("fapped-btn").disabled = false //keep it true
 document.getElementById("saved-btn").disabled = false //keep it true
 
+//localStorage.clear(); /////////////////////////DANGER
+
+
+
+
+
+
+clock()
+timer()
 checkSavedArray() //  {    DO NOT
 retrieveData()    //  {  CHANGE THE
 buildTable(arr)   //  { ORDER OF THESE
+
+
 
 //|||||||||||||||||||||||||||->>  THE FUNCTIONLAND  <<-|||||||||||||||||||||||||
 
@@ -60,37 +76,13 @@ function addTable(status) { // Pushes the data to ARRAY and stores the array loc
             status: status,
             percent: percent.toFixed(2),
             fappedNumber: fappedNumber,
-            savedNumber: savedNumber
+            savedNumber: savedNumber,
+            dateNumber: dateNumberJS,
+            monthNumber: monthNumberJS
         }
     )
     buildTable(arr)
     localStorage.setItem("progressArray", JSON.stringify(arr));
-}
-
-function buildTable(arr) { // Pulls Data from ARRAY and Builds the table rows
-    console.log("Reached buildTable")
-    tbodyEl.innerHTML = `<tr>
-                            <th colspan="3">Daily Progress 🚧</th>
-                        </tr>
-                        <tr>
-                            <td><b>Days</b></td>
-                            <td><b>Status</b></td>
-                            <td><b>Percentage</b></td>
-                        </tr>`
-    // For loop
-    for (var i = 0; i < arr.length; i++) {
-        console.log("Reached buildTable loop")
-        var row = `<tr>
-                        <td>Day ${arr[i].day}</td>
-                        <td> ${arr[i].status}</td>
-                        <td> ${arr[i].percent}</td>
-                  </tr>`
-        tbodyEl.innerHTML += row
-    }
-    smallTableEl.innerHTML = `<tr>
-                                <td>Slips = ${fappedNumber}</td>
-                                <td>Saves = ${savedNumber}</td>
-                              </tr>`
 }
 
 function disableBtn() { // Disables the buttons (when outside the time limit)
@@ -105,6 +97,13 @@ function enableBtn() { // Enables the buttons (when inside the time limit)
     document.getElementById("demo").innerHTML = "Buttons are Enabled"
 }
 
+function checkSavedArray() { // Checks if the local array exists or not, if not creates an empty local array
+    if (localStorage.getItem("progressArray") == null) {
+        localStorage.setItem("progressArray", "[]")
+        console.log("Locale is null")
+    }
+}
+
 function retrieveData() { // Retrieves data from local storage and assigns it to respective variables
     savedData = JSON.parse(localStorage.getItem("progressArray"));
     arr = savedData
@@ -116,13 +115,53 @@ function retrieveData() { // Retrieves data from local storage and assigns it to
 
     daysCountEl.innerText = "~ DAY " + (dayNumber + 1) + " ~" // to update the html upon starting
     percentEl.innerText = "Success Rate📈: " + percent + "%"
+
+    //Making sure you only get a single chance a day
+    singleChance()
 }
 
-function checkSavedArray() { // Checks if the local array exists or not, if not creates an empty local array
-    if (localStorage.getItem("progressArray") == null) {
-        localStorage.setItem("progressArray", "[]")
-        console.log("Locale is null")
+function buildTable(arr) { // Pulls Data from ARRAY and Builds the table rows
+    console.log("Reached buildTable")
+    tbodyEl.innerHTML = `<tr>
+                            <th colspan="3">Daily Progress 🚧</th>
+                        </tr>
+                        <tr>
+                            <td><b>Day</b></td>
+                            <td><b>Status</b></td>
+                            <td><b>Percentage</b></td>
+                        </tr>`
+    // For loop
+    for (var i = 0; i < arr.length; i++) {
+        console.log("Reached buildTable loop")
+        var row = `<tr>
+                        <td> ${arr[i].dateNumber} ${months[arr[i].monthNumber]}</td>
+                        <td> ${arr[i].status}</td>
+                        <td> ${arr[i].percent}</td>
+                  </tr>`
+        tbodyEl.innerHTML += row
     }
+    smallTableEl.innerHTML = `<tr>
+                                <td>Slips = ${fappedNumber}</td>
+                                <td>Saves = ${savedNumber}</td>
+                              </tr>`
+}
+
+function singleChance() {  //Making sure you only get a single chance a day
+    let dateCheck = savedData[savedData.length - 1].dateNumber
+    let monthCheck = savedData[savedData.length - 1].monthNumber
+    if (dateNumberJS == dateCheck && monthNumberJS == monthCheck) {
+        console.log("Dates matched 1")
+        console.log(dateNumberJS, months[monthNumberJS])
+        disableBtn()
+        document.getElementById("demo").innerHTML = "You had ur chance blud"
+    }
+}
+
+function clr() {
+    alert("ur deleting all the saved data\nu ok bro?")
+    console.log("pressed")
+    localStorage.clear();
+    location.reload()
 }
 
 //|||||||||||||||||||||||||||>>  TIMER ALGORITHM  <<|||||||||||||||||||||||||//
@@ -198,10 +237,6 @@ function timerActive() {
         }
     }, 1000);
 }
-
-
-
-
 function block(x) {
     clearInterval(x);
     document.getElementById("fapped-btn").disabled = true;
@@ -213,35 +248,35 @@ function block(x) {
 
 //-------------- ALTERNATE APPROACH WITHOUT THE COUNTDOWN >
 
-var checkHour = new Date().getHours()
-var checkMinutes = new Date().getMinutes()
-var hourNum = 18 // Give the Hour Number here
-if (checkHour == hourNum) {
-    console.log("if> true")
-    console.log(checkHour, checkMinutes)
-    enableBtn()
-} else if (checkHour < hourNum) {
-    console.log("if> false, moved to else if")
-    console.log(checkHour, checkMinutes)
-    disableBtn()
-    document.getElementById("demo").innerHTML = "Buttons are Disabled until 5pm Today"
-} else if (checkHour > hourNum) {
-    console.log("if> false, moved to else if 2")
-    console.log(checkHour, checkMinutes)
-    disableBtn()
-    document.getElementById("demo").innerHTML = "Buttons are Disabled until 5pm Tomorrow"
+function timer() {
+    var checkHour = new Date().getHours()
+    var checkMinutes = new Date().getMinutes()
+    var hourNum = 17 // Give the Hour Number here
+    if (checkHour == hourNum) {
+        console.log("if> true")
+        console.log(checkHour, checkMinutes)
+        enableBtn()
+    } else if (checkHour < hourNum) {
+        console.log("if> false, moved to else if")
+        console.log(checkHour, checkMinutes)
+        disableBtn()
+        document.getElementById("demo").innerHTML = "Buttons are Disabled until 5pm Today"
+    } else if (checkHour > hourNum) {
+        console.log("if> false, moved to else if 2")
+        console.log(checkHour, checkMinutes)
+        disableBtn()
+        document.getElementById("demo").innerHTML = "Buttons are Disabled until 5pm Tomorrow"
+    }
 }
 
-
 // ---------------------- CLOCK ----------------------
-setInterval(function () {
-    var time = new Date();
-    var h = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: "numeric", hour12: true })
-    timeNowEl.innerHTML = h
-}, 1000)
-
+function clock() {
+    setInterval(function () {
+        var time = new Date();
+        var h = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: "numeric", hour12: true })
+        timeNowEl.innerHTML = h
+    }, 1000)
+}
 console.log("last line")
 
 // ---------------- The Sandbox --------------
-
-
